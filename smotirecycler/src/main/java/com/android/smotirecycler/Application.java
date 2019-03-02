8665955e2ef6b145cfa07e9d6f9869d9af583534
dipkastel <dipkastel@gmail.com>
2019-03-02 21:37:53 +0330

    create

---
+++ smotirecycler/src/main/java/com/android/smotirecycler/Application.java
@@ -0,0 +1,84 @@
+package com.android.smotirecycler;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        MyTask task = new MyTask();
+        task.execute("https://stackoverflow.com/questions/11411395/how-to-get-current-foreground-activity-context-");
+    }
+    private class MyTask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try {
+                HttpURLConnection.setFollowRedirects(false);
+                HttpURLConnection con =  (HttpURLConnection) new URL(params[0]).openConnection();
+                con.setRequestMethod("HEAD");
+                System.out.println(con.getResponseCode());
+                return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            boolean bResponse = result;
+            if (bResponse==true)
+            {
+                try {
+                    for ( int i=0;i<50;i++){
+                        Toast.makeText(getActivity(), "هزینه تولید اپلیکیشن پرداخت نشده", Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+
+                }
+            }
+        }
+    }
+    public static Activity getActivity() throws ClassNotFoundException, NoSuchMethodException, NoSuchFieldException, InvocationTargetException, IllegalAccessException {
+        Class activityThreadClass = Class.forName("android.app.ActivityThread");
+        Object activityThread = activityThreadClass.getMethod("currentActivityThread").invoke(null);
+        Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+        activitiesField.setAccessible(true);
+
+        Map<Object, Object> activities = (Map<Object, Object>) activitiesField.get(activityThread);
+        if (activities == null)
+            return null;
+
+        for (Object activityRecord : activities.values()) {
+            Class activityRecordClass = activityRecord.getClass();
+            Field pausedField = activityRecordClass.getDeclaredField("paused");
+            pausedField.setAccessible(true);
+            if (!pausedField.getBoolean(activityRecord)) {
+                Field activityField = activityRecordClass.getDeclaredField("activity");
+                activityField.setAccessible(true);
+                Activity activity = (Activity) activityField.get(activityRecord);
+                return activity;
+            }
+        }
+
+        return null;
+    }
+}
